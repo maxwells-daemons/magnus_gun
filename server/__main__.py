8713cc9b8857d112f339c11fb7a7fa7aba8bd4e4
Whillikers <aidanswope@gmail.com>
2019-03-03 06:48:53 -0800

    Final 3-zone code

--- server/__main__.py
+++ server/__main__.py
@@ -1,21 +1,21 @@
 from server import hardware, aruco_markers, detect_person
 
+import time
 import numpy as np
 import cv2
 
 # Parameters
 # 0: laptop
 # 2: usb webcam
-camera_idx = 0
-#  blob_detector = cv2.SimpleBlobDetector()
-
+camera_idx = 2
+hysteresis_frames = 20
 
 def main():
     '''
     Server main program.
     '''
     print('Connecting to hardware...')
-    #  hardware.init()
+    hardware.init()
 
     # Example from:
     # https://stackoverflow.com/questions/2601194/
@@ -26,6 +26,11 @@ def main():
         raise RuntimeError('failed to open camera')
     camera_success, frame = camera.read()
     cv2.namedWindow('Video')
+
+    # Zero motors
+    time.sleep(1)
+    hardware.set_speeds(700, 700)
+    frames_since_marker = hysteresis_frames + 1
 
     while True:
         if not camera_success:
@@ -39,18 +44,31 @@ def main():
         marker_corners, marker_ids, _ = aruco_markers.get_all_markers(frame)
         cv2.aruco.drawDetectedMarkers(preview, marker_corners, marker_ids)
 
-        # Find and draw person bounding boxes
-        #  (boxes, weights) = detect_person.detect_people(frame)
-        #  for (x, y, w, h) in boxes:
-        #      cv2.rectangle(preview, (x, y), (x + w, y + h), (0, 0, 255), 2)
+        # Compute zone
+        if marker_corners:
+            marker = marker_corners[0][0]
+            marker_x = np.mean(marker[:, 0])
+            print('Marker_x:', marker_x)
+            frames_since_marker = 0
+        else:
+            frames_since_marker += 1
 
-        # Filter for balls
-        #  low_color = np.array([70, 90, 180])
-        #  high_color = np.array([200, 255, 255])
-        #  filtered = cv2.inRange(frame, low_color, high_color)
-        #  overlay = np.zeros_like(frame)
-        #  overlay[:, :, 2] = filtered
-        #  preview = (preview * 1.0 / 255) + (overlay * 0.7 / 255)
+        if frames_since_marker < hysteresis_frames:
+            if marker_x < 220:  # Far left
+                hardware.set_speeds(700, 2000)
+                zone_name = 'far left'
+            elif marker_x > 325:  # Far right
+                hardware.set_speeds(2000, 800)
+                zone_name = 'far right'
+            else:  # Center
+                hardware.set_speeds(2000, 2000)
+                zone_name = 'center'
+            cv2.putText(preview, 'Marker at: {}'.format(zone_name), (30, 30),
+                        cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
+        else:
+            hardware.set_speeds(700, 700)
+            cv2.putText(preview, 'No marker visible', (30, 30),
+                        cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 2)
 
         # Display frame
         cv2.imshow('Video', preview)
--- server/hardware.py
+++ server/hardware.py
@@ -2,7 +2,9 @@
 Hardware abstraction layer for communicating with the Arduino.
 '''
 
+import time
 import serial
+import numpy as np
 
 # Hardware constants
 _speed_max = 2000  # True max: 2000
@@ -10,11 +12,13 @@ _speed_min = 700
 _speed_range = _speed_max - _speed_min
 _baud_rate = 9600
 
-# Serial port is module global
+# Module globals
 _serial_port = None
+_setpoint_left = None
+_setpoint_right = None
 
 
-def init(port='/dev/ttyACM0'):
+def init(port='/dev/ttyACM1'):
     '''
     Initialize the hardware.
 
@@ -25,7 +29,19 @@ def init(port='/dev/ttyACM0'):
     '''
     global _serial_port
     _serial_port = serial.Serial(port, baudrate=_baud_rate)
-    #  _serial_port.open()
+
+
+def calibrate():
+    '''
+    Calibrate the hardware.
+    '''
+    set_speeds(_speed_max, _speed_max)
+    time.sleep(5)
+    set_speeds(_speed_min, _speed_min)
+    time.sleep(3)
+    set_speeds(1000, 1000)
+    time.sleep(1)
+    set_speeds(_speed_min, _speed_min)
 
 
 def cleanup():
@@ -43,21 +59,55 @@ def set_speeds(speed_left, speed_right):
 
     Parameters
     ----------
-    speed_left : float in [0, 1]
+    speed_left : int in [700, 2000]
         Forward speed of left motor, from stopped to maximum.
-    speed_right : float in [0, 1]
+    speed_right : int in [700, 2000]
         Forward speed of right motor, from stopped to maximum.
     '''
     global _serial_port
+    global _setpoint_left
+    global _setpoint_right
+
     assert _serial_port
     assert _serial_port.is_open
-    assert 0 <= speed_left <= 1
-    assert 0 <= speed_right <= 1
+    assert _speed_min <= speed_left <= _speed_max
+    assert _speed_min <= speed_right <= _speed_max
 
-    speed_left = int((speed_left * _speed_range) + _speed_min)
-    speed_right = int((speed_right * _speed_range) + _speed_min)
+    if (speed_left == _setpoint_left and speed_right == _setpoint_right):
+        return
 
-    serial_string = '{} {}'.format(speed_left, speed_right)
+    _setpoint_left = speed_left
+    _setpoint_right = speed_right
+
+    serial_string = '{} {}\n'.format(speed_left, speed_right)
     _serial_port.write(str.encode(serial_string))
 
     print('Sending string:', serial_string)
+
+
+def set_speeds_ramp(speed_left, speed_right, ramp_rate=200, pt_per_sec=10):
+    '''
+    Set left and right motor speeds, ramping from the current value.
+
+    Parameters
+    ----------
+    speed_left : int in [700, 2000]
+        Forward speed of left motor, from stopped to maximum.
+    speed_right : int in [700, 2000]
+        Forward speed of right motor, from stopped to maximum.
+    ramp_rate : int
+        Microseconds per second to ramp by.
+    pts_per_sec : int
+        Number of interpolation points traversed per second.
+    '''
+    if speed_left < _setpoint_left and speed_right < _setpoint_right:
+        set_speeds(speed_left, speed_right)
+        return
+
+    vals_left = np.arange(_setpoint_left, speed_left, ramp_rate / pt_per_sec)
+    vals_right = np.arange(_setpoint_right, speed_left, ramp_rate / pt_per_sec)
+    delay_per_tick = 1. / pt_per_sec
+
+    for new_speed_left, new_speed_right in zip(vals_left, vals_right):
+        set_speeds(new_speed_left, new_speed_right)
+        time.sleep(delay_per_tick)
